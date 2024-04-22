6dbf6a37c78f41a1267e3c6dad9c6d7d876c6fbe
1tpp <panapat.dev@gmail.com>
2024-04-22 17:22:31 +0700

    Add Product component and render mock data in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import mockData from "./constants/mock.json";
+
+function Product({ data }) {
+  return (
+    <div
+      style={{
+        border: "1px solid #000",
+        padding: "1rem",
+      }}
+    >
+      <h1>Model: {data.model}</h1>
+      <img src={data.modelImage} alt="model image" width={250} height={250} />
+      <h1>{data.baseSpecification.processor}</h1>
+      <h1>{data.baseSpecification.graphicProcessor}</h1>
+      <h1>{data.baseSpecification.momory}</h1>
+      <h1>{data.baseSpecification.storageCapacity}</h1>
+
+      <p>{data.modelColorName}</p>
+      <p>{data.features.neural_engine}</p>
+      <p>{data.features.display}</p>
+      <p>{data.features.camera}</p>
+      <p>{data.features.ports}</p>
+      <p>{data.features.power_adapter}</p>
+      <p>{data.features.external_display_support}</p>
+      <p>{data.features.keyboard}</p>
+      <p>{data.features.trackpad}</p>
+      <p>{data.features.power_adapter}</p>
+    </div>
+  );
+}
+
 function App() {
-  return <></>;
+  return (
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
+      {mockData.map((item) => (
+        <Product data={item} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
